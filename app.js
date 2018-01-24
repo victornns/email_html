e250fe7fc61d3388e320a47b8c5593e11f681f71
Victor Nascimento <victor.nascimento.ns@gmail.com>
2018-01-24 11:10:50 -0200

    one_at_a_time file

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const inlineCss = require('inline-css')
-const fs = require('fs')
+const fs        = require('fs')
+const concat    = require('concat')
 
 
-one_at_a_time('users_new_access.html')
+// compile single file or undefined will compile all files
+// let single_file = 'example_file.html'   /   undefined
+let single_file = 'users_new_access.html'
+
+
+if(single_file)
+    one_at_a_time(single_file)
+else
+    all_at_once()
+
+
+
+
 
 
 
 function one_at_a_time(file){
-    let data = fs.readFileSync('./template/' + file, 'utf8', (err, data) => {
-        if (err) throw err
+
+    concat([
+        './template/partials/header.html',
+        './template/' + file,
+        './template/partials/footer.html'
+    ]).then(result => {
+        let data = result
+
+        inlineCss(data, {url: 'file://' + __dirname + '/template/css/style.css'})
+            .then(function(data) {
+                fs.writeFile('./output/' + file, data, ()=>{})
+            })
     })
 
-    inlineCss(data, {url: 'file://' + __dirname + '/template/css/style.css'})
-        .then(function(data) {
-            console.log(data)
-        })
+
 }
 
 
-// function all_at_once(file){
-//
-// }
+function all_at_once(){
+    console.log('all');
+}
